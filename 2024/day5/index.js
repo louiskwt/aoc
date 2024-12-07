ed455b22001ab7bd0da6292a4a7039d68b157746
Louis Tsang <kawingt2@gmail.com>
2024-12-07 11:52:21 +0800

    finished 2024 day5 part 2

--- 2024/day5/index.js
+++ 2024/day5/index.js
@@ -34,7 +34,11 @@ const correctUpdates = updates.filter((u) => {
   return u.every(checkUpdate);
 });
 
-console.log(correctUpdates);
+const incorrectUpdates = updates.filter((u) => {
+  return !u.every(checkUpdate);
+});
+
+// console.log(correctUpdates);
 
 const sum = correctUpdates.reduce((acc, curr) => {
   acc += curr[Math.floor(curr.length / 2)];
@@ -43,14 +47,58 @@ const sum = correctUpdates.reduce((acc, curr) => {
 
 console.log(sum);
 
+// console.log(incorrectUpdates);
+
+// const misplacedArr = incorrectUpdates.map((u, _, arr) => {
+//   const misplacedArr = [];
+//   for (const num of u) {
+//     const before = instructioins.filter((i) => {
+//       return i.indexOf(num) === 1;
+//     });
+
+//     before.map((n) => {
+//       if (u.includes(n[0]) && u.indexOf(n[0]) > u.indexOf(num)) {
+//         misplacedArr.push(n);
+//       }
+//     });
+//   }
+//   return misplacedArr;
+// });
+
+// console.log(misplacedArr);
+
+// const fixedArr = incorrectUpdates.map((u, index, arr) => {
+//   const updatedArr = [...u];
+//   do {
+//     for (const pair of misplacedArr[index]) {
+//       const first = pair[0];
+//       const second = pair[1];
+//       const firstIndex = updatedArr.indexOf(first);
+//       const secondIndex = updatedArr.indexOf(second);
+//       const tmp = second;
+//       updatedArr[secondIndex] = first;
+//       updatedArr[firstIndex] = tmp;
+//     }
+//   } while (!updatedArr.every(checkUpdate));
+
+//   return updatedArr;
+// });
+
+// console.log(fixedArr);
+
+// const part2Sum = fixedArr.reduce((acc, curr) => {
+//   acc += curr[Math.floor(curr.length / 2)];
+//   return acc;
+// }, 0);
+
+// console.log(part2Sum);Vk
+
 function checkUpdate(num, _, arr) {
   const before = instructioins
     .filter((i) => {
       return i.indexOf(num) === 1;
     })
     .map((i) => i[0]);
-
-  // console.log("before: ", before, arr);
 
   if (before.length === 0) return true;
 
